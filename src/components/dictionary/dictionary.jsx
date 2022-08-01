d67eac817734f87fc79f5f6b49daed18e5d07cfb
adik-creator <adduser1000@gmail.com>
2022-08-02 04:17:54 +0600

    new styles

--- src/components/dictionary/dictionary.jsx
+++ src/components/dictionary/dictionary.jsx
@@ -7,10 +7,8 @@ const Dictionary = ({dictionary}) => {
             <div className="cardMedia">
                 <img src={dictionary.img} alt="image" className="cardImage"/>
             </div>
-            <div className="cardContentEnglish">
+            <div className="cardContent">
                 <h3 className="cardEnglish">{dictionary.english}</h3>
-            </div>
-            <div className="cardContentRussian">
                 <h4 className="cardRussian">{dictionary.russian}</h4>
             </div>
         </div>
